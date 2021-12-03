fdb5434d35fff2299df96971023ddf0932262100
Daniel-Podsadowski <35496162+Daniel-Podsadowski@users.noreply.github.com>
2021-12-02 20:44:07 -0500

    Setup Day 3

---
+++ AdventOfCode2021/Day/Day3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2021
+{
+    public static class Day3
+    {
+        public static void Main()
+        {
+            var fileInput = @"..\..\..\Data\day3.txt";
+            var lines = File.ReadAllLines(fileInput);
+
+            Console.WriteLine("Commencing Day 3 Part A...");
+            
+            Console.WriteLine("Answer: ");
+
+            Console.WriteLine("Commencing Day 3 Part B...");
+
+            Console.WriteLine("Answer: ");
+        }
+    }
+}
+
