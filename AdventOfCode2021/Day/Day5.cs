05dbe30a59c1d3eabc3913aca9999707fbdbc612
Daniel-Podsadowski <35496162+Daniel-Podsadowski@users.noreply.github.com>
2021-12-05 17:49:06 -0500

    Prepared for Day5

---
+++ AdventOfCode2021/Day/Day5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode2021
+{
+    public static class Day5
+    {
+        public static void Main()
+        {
+            var fileInput = @"..\..\..\Data\Test.txt";
+            var lines = File.ReadAllLines(fileInput);
+
+            Console.WriteLine("Commencing Day 3 Part A...");
+            
+
+            Console.WriteLine("Answer: " );
+
+            Console.WriteLine("Commencing Day 3 Part B...");
+
+
+            Console.WriteLine("Answer: ");
+        }
+    }
+}
+
