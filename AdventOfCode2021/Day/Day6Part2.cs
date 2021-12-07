5aec8e12d82acaf51dc64299dbcc3b9afb6b4b06
Daniel-Podsadowski <35496162+Daniel-Podsadowski@users.noreply.github.com>
2021-12-07 14:59:41 -0500

    Completed Day6 Part 2

--- AdventOfCode2021/Day/Day6Part2.cs
+++ AdventOfCode2021/Day/Day6Part2.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace AdventOfCode2021
 {
@@ -8,14 +8,33 @@ namespace AdventOfCode2021
     {
         public static void Main()
         {
-            var fileInput = @"..\..\..\Data\Test.txt";
-            var lines = File.ReadAllLines(fileInput);
+            var fileInput = @"..\..\..\Data\Day6.txt";
+            var input = File.ReadAllLines(fileInput)[0].Split(',').Select(int.Parse).ToList();
 
-            var fishList = lines[0].Split(',').Select(int.Parse).ToList();
+            long[] fishies = new long[9];
+            int days = 256;
 
-            //I can't do it ;(
+            for(int i = 0; i < input.Count; i++)
+            { 
+                fishies[input[i]]++;
+            }
 
-            Console.WriteLine("Answer: " + fishList.Length);
+            for(int i = 0; i < days; i++)
+            {
+                long tmpFish = fishies[0];
+                Array.Copy(fishies, 1, fishies, 0, fishies.Length - 1);
+
+                fishies[6] += tmpFish;
+                fishies[8] = tmpFish;
+            }
+
+            long count = 0;
+            for (int i = 0; i < fishies.Length; i++)
+            { 
+                count += fishies[i];
+            }
+
+            Console.WriteLine("Answer: " + count);
         }
     }
 }
