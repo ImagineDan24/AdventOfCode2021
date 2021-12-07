f5151ea8d39ad5ddf9a02728906ce0f6c221fb3a
Daniel-Podsadowski <35496162+Daniel-Podsadowski@users.noreply.github.com>
2021-12-07 10:33:59 -0500

    Finished Day 7

--- AdventOfCode2021/Day/Day7Part2.cs
+++ AdventOfCode2021/Day/Day7Part2.cs
@@ -9,10 +9,31 @@ namespace AdventOfCode2021
     {
         public static void Main()
         {
-            var fileInput = @"..\..\..\Data\Test.txt";
-            var lines = File.ReadAllLines(fileInput);
+            var fileInput = @"..\..\..\Data\Day7.txt";
+            var input = File.ReadAllLines(fileInput)[0].Split(',').Select(int.Parse).ToList();
 
+            int bestFuel = 1000000000;
+            for (int i = 0; i < input.Max(); i++)
+            {
+                int fuel = 0;
+                for (int j = 0; j < input.Count; j++)
+                {
+                    int step = 0;
+                    int low; int high;
+                    if (input[j] > i) { low = i; high = input[j]; }
+                    else { low = input[j]; high = i; }
 
+                    for (int k = low; k < high; k++)
+                    {
+                        fuel += 1 + step;
+                        step++;
+                    }
+                }
+
+                if (fuel < bestFuel) { bestFuel = fuel; }
+            }
+
+            Console.WriteLine("Answer: " + bestFuel);
         }
     }
 }
