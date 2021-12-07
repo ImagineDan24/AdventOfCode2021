011884dfa9269be2176cbbc6d4462c465d56d7ae
Daniel-Podsadowski <35496162+Daniel-Podsadowski@users.noreply.github.com>
2021-12-07 13:32:03 -0500

    Prepared for Day 8

---
+++ AdventOfCode2021/Day/Day8Part1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2021
+{
+    public static class Day8Part1
+    {
+        public static void Main()
+        {
+            var fileInput = @"..\..\..\Data\Test.txt";
+            var lines = File.ReadAllLines(fileInput);
+
+            Console.WriteLine("Answer: ");
+        }
+    }
+}
